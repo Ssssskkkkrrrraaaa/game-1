307374309accaca4c604a10298a6923dfc6dbf4a
Ssssskkkkrrrraaaa <110767617+Ssssskkkkrrrraaaa@users.noreply.github.com>
2022-08-19 15:52:21 +0500

    Update AuraTrade_miniGame-HTML5-Shipping.UE4.js

--- AuraTrade_miniGame-HTML5-Shipping.UE4.js
+++ AuraTrade_miniGame-HTML5-Shipping.UE4.js
@@ -1155,7 +1155,7 @@ $(document).ready(function() {
 		// ----------------------------------------
 		// MORE JS
 		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Ssssskkkkrrrraaaa/game-1/d1bd1f407e443473cd147e295af16723dae142c1/AuraTrade_miniGame-HTML5-Shipping.data.js'));
-		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
+		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Ssssskkkkrrrraaaa/game-1/bff85b33c88ea7f04e6150fb282035b1bb757a45/Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
 			fetchOrDownloadAndStore(db, Module.locateFile('AuraTrade_miniGame-HTML5-Shipping.data')).then(function(dataBlob) {
